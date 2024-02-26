69ee6f528370140e2ff4fd570072384813a7b8d5
mekonical22 <mekonical222@gmail.com>
2024-02-26 17:53:56 +0800

    open file

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	filepath := os.Args[1]
+	if filepath == "" {
+		fmt.Println("no path provided")
+		os.Exit(1)
+	}
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	defer file.Close()
+}
